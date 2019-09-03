9f61a0c13e23606d4bced6975e06c2246ed8c752
Mystic67 <webmaster@passion-geek.com>
2019-09-03 19:42:08 +0200

    add test for TextParcer

---
+++ tests/test_parser.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+from grandpybotapp.utils.textParser import TextParser
+import pytest
+import nltk
+
+
+
+textList = ["Salut GrandPy ! Est-ce que tu connais OpenClassrooms ?",
+        "Que peux-tu me dire sur Kuala Lumpur ?",
+        "As-tu des informations sur le musé du Louvre ?",
+        "Je voudrais visiter le Mont saint Odile",
+        "Bonjour Papy ! As-tu connu les tours jumelles?",
+        "Je voudrais me rendre rue de la première armée à Strasbourg."]
+
+parsedTextList = ["OpenClassrooms Paris",
+            "Kuala Lumpur",
+            "informations musé Louvre",
+            "Mont saint Odile",
+            "tours jumelles",
+            "rue armée Strasbourg"]
+
+def test_parse_the_text():
+    for i, text in enumerate(textList):
+        assert TextParser.parse_text(text) == parsedTextList[i]
