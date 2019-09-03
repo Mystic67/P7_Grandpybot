12dfe233016f415f2e7a001edcbf7d682fac1938
Mystic67 <webmaster@passion-geek.com>
2019-09-03 19:38:36 +0200

    change the TextParcer to use it as @classmethod

--- grandpybotapp/utils/textParser.py
+++ grandpybotapp/utils/textParser.py
@@ -1,20 +1,22 @@
 #! /usr/bin/env python
 # coding: utf-8
 
-from .settings import config as constants
+from .settings import texts_config as constants
 import nltk
 
 class TextParser:
-
-    def __init__(self):
-        self.stopwords = constants.STOPWORDS + constants.CUSTOM_STOPWORDS
-
-    def parse_text(self, text):
+    ''' This module parse the text from userMessage'''
+    @classmethod
+    def parse_text(cls, text):
+        stopwords = constants.STOPWORDS + constants.CUSTOM_STOPWORDS
         parsedListText =[]
         tokenizer = nltk.RegexpTokenizer(r'\w+')
         tokenized_text = tokenizer.tokenize(text)
         for word in tokenized_text:
-            if word.lower() not in self.stopwords:
+            if word.lower() not in stopwords:
                 parsedListText.append(word)
+            if "openclassrooms" in word.lower():
+                parsedListText.append("Paris")
         parsedWords = " ".join(parsedListText)
+        print(parsedWords)
         return parsedWords
