52067ac084535817835a6a689c1529db735742ff
Mystic67 <webmaster@passion-geek.com>
2019-09-10 13:58:47 +0200

    reviews all the mocks for mediawiki tests

--- tests/test_mediawiki.py
+++ tests/test_mediawiki.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # coding: utf-8
 
-from grandpybotapp.utils import mediawiki
+from grandpybotapp.utils.mediawiki import MediaWiki
 import requests
 import json
 import pytest
@@ -10,7 +10,6 @@ import pytest
 mediaWikiIdRequestsResult = {'query': {'search': [{'pageid': '4338589'}] } }
 
 idResult = "4338589"
-
 
 #requests query result look like
 mediawikiTextRequestsResult = {"query" :
@@ -26,37 +25,86 @@ infosResult = {'status': 'OK',
                 'text': 'OpenClassrooms est une école ...'
             }
 
-# instance mediawiki
-mediawiki_instance = mediawiki.MediaWiki('OpenClassrooms Paris')
+# will mock the requests.get methode
+# custom class to be the mock return value
+class MockRequestsGet:
+    '''This class will mock the requests.get method'''
+    def __init__(self, status_code, response):
+        self.status_code = status_code
+        self.response = response
+    def json(self):
+        return self.response
 
-class MockResponseId:
-    @staticmethod
-    def json():
-        return mediaWikiIdRequestsResult
+def test_get_id_parsing_with_results(monkeypatch):
+    ''' This function test if get_id parsing response is ok when requests result is ok '''
+    # instance mock requests.get class with params
+    mockRequestsGet = MockRequestsGet(200, mediaWikiIdRequestsResult)
+    def mock_response(*url, **params):
+        return mockRequestsGet
 
-class MockResponseInfos:
-    @staticmethod
-    def json():
-        return mediawikiTextRequestsResult
-
-def test_get_id_is_ok(monkeypatch):
-    # custom class to be the mock return value
-    # will override the requests.Response returned from requests.get
-    def mock_get_id_get(*url, **params):
-        return MockResponseId
-    #patch method "get_place_infos" with "mock_get_id"
-    monkeypatch.setattr(requests,'get', mock_get_id_get)
+    #print(mock_response().json())
+    #patch method get with mock_response
+    monkeypatch.setattr(requests,'get', mock_response)
     # "get_id", uses the monkeypatch
-    assert mediawiki_instance.get_id() == idResult
+    assert MediaWiki('fake_place').get_id() == idResult
 
-def test_get_infos_is_ok(monkeypatch):
-    # custom class to be the mock return value
-    # will override the requests.Response returned from requests.get
-    def mock_get_infos_get(*url, **params):
-        return MockResponseInfos
+def test_get_infos_parsing_with_result(monkeypatch):
+    ''' This function test if get_infos parsing response is ok when requests response has result'''
+    # instance mock requests.get class with params
+    mockRequestsGet = MockRequestsGet(200, mediawikiTextRequestsResult)
+    def mock_response(*url, **params):
+        return mockRequestsGet
 
-    #patch method "get_place_infos" with "mock_get_id"
-    monkeypatch.setattr(requests,'get', mock_get_infos_get)
+    print(mock_response().json())
+    #patch method get with mock_response
+    monkeypatch.setattr(requests,'get', mock_response)
+    # Test responses
+    print(MediaWiki('OpenClassrooms Paris').get_infos())
+    assert MediaWiki('OpenClassrooms Paris').get_infos() == infosResult
+    assert MediaWiki('fake_place').infos == infosResult
 
-    # "get_infos", uses the monkeypatch
-    assert mediawiki_instance.get_infos() == infosResult
+def test_get_id_parsing_with_no_results(monkeypatch):
+    ''' This function test if get_id parsing response is ok when requests result has no result '''
+    # instance mock requests.get class with params
+    mockRequestsGet = MockRequestsGet(200, {"query":{"search":[]}})
+    def mock_response(url, params= None):
+        return mockRequestsGet
+    #patch method get with mock_response
+    monkeypatch.setattr(requests,'get', mock_response)
+    # Test responses
+    assert MediaWiki('fake_place').get_id() == ""
+
+def test_get_infos_parsing_with_no_result(monkeypatch):
+    ''' This function test if get_infos parsing response is ok when requests response has no result'''
+    # instance mock requests.get class with params
+    mockRequestsGet = MockRequestsGet(200, {"query":{"search":[]}})
+    def mock_response(url, params= None):
+        return mockRequestsGet
+    #patch method get with mock_response
+    monkeypatch.setattr(requests,'get', mock_response)
+    # Test responses
+    assert MediaWiki('fake_place').get_infos() == {'status': 'NOT FOUND'}
+    assert MediaWiki('fake_place').infos == {'status': 'NOT FOUND'}
+
+def test_get_id_parsing_with_error_404(monkeypatch):
+    ''' This function test if get_id parsing response if url is not correct or service not accessible  '''
+    # instance mock requests.get class with params
+    mockRequestsGet = MockRequestsGet(404, {"query":{"search":[]}})
+    def mock_response(url, params= None):
+        return mockRequestsGet
+    #patch method get with mock_response
+    monkeypatch.setattr(requests,'get', mock_response)
+    # Test responses
+    assert MediaWiki('fake_place').get_id() == ""
+
+def test_get_infos_parsing_with_error_404(monkeypatch):
+    ''' This function test get_infos parsing response if url is not correct or service not accessible'''
+    # instance mock requests.get class with params
+    mockRequestsGet = MockRequestsGet(404, {"query":{"search":[]}})
+    def mock_response(url, params= None):
+        return mockRequestsGet
+    #patch method get with mock_response
+    monkeypatch.setattr(requests,'get', mock_response)
+    # Test responses
+    assert MediaWiki('fake_place').get_infos() == {'status': 'error 404'}
+    assert MediaWiki('fake_place').infos == {'status': 'error 404'}
